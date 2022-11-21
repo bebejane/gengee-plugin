955fed7683ae95a3782b7150c4cd338059f39596
Bebe Jane <bjornthief@gmail.com>
2022-11-21 16:54:51 +0100

    fix base63 encode/decode

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,12 @@
 export const isDev = document.location.hostname === 'localhost';
 export const baseUrl = 'https://social-gen.vercel.app'
 
+export const Base64 = {
+  encode: (obj: any): string => btoa(unescape(encodeURIComponent(JSON.stringify(obj)))),
+  decode: (str: string): any => JSON.parse(decodeURIComponent(escape(atob(str))))
+}
+
 export const generateSourceUrl = (template: any, { fields }: { fields: Fields }) => {
   const { config } = template;
-  return `${baseUrl}/api/generate?t=${config.id}&f=${btoa(JSON.stringify(fields))}`
+  return `${baseUrl}/api/generate?t=${config.id}&f=${Base64.encode(fields)}`
 }
